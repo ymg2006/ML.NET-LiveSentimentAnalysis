268709bf29d9cd95e5a7541bdf7288dd08111223
ymg2006 <f.partonia@gmail.com>
2021-08-16 15:31:33 +0430

    Add project files.

---
+++ BlazorSentiment.Server/ML/DataModels/SamplePrediction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace BlazorSentiment.Server.ML.DataModels
+{
+    public class SamplePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Score { get; set; }
+    }
+}
+
+
+
---
+++ BlazorSentiment.Server/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using Microsoft.Extensions.ML;
+using BlazorSentiment.Server.ML.DataModels;
+
+namespace BlazorSentiment.Server
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddNewtonsoftJson();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+
+            services.AddPredictionEnginePool<SampleObservation, SamplePrediction>()
+                    .FromFile(Configuration["MLModel:MLModelFilePath"]);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBlazorDebugging();
+            }
+
+            app.UseRouting();
+            app.UseClientSideBlazorFiles<Client.Startup>();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+            });
+        }
+    }
+}
